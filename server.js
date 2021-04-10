0d9213c2389ea20183ef16d51fe964d1bf69c0ac
Lyle Kilson <lylekilsongithub@gmail.com>
2021-04-09 18:46:57 -0600

    Set up GET for zookeeper

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const fs = require("fs");
-
 const path = require("path");
-
+const express = require("express");
 const { animals } = require("./data/animals");
 
-const express = require("express");
-const { type } = require("os");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 // parse incoming JSON data
 app.use(express.json());
@@ -67,23 +64,6 @@ function findById(id, animalsArray) {
   return result;
 }
 
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get("/api/animals/:id", (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
 function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
@@ -107,19 +87,52 @@ function validateAnimal(animal) {
   if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
     return false;
   }
+  return true;
 }
+
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 err back
   if (!validateAnimal(req.body)) {
     res.status(400).send("The animal is not properly formatted.");
   } else {
     const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
+    res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
